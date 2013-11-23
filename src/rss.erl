03d551207ea15525e37666dff269981e2d4af960
Michael Santos <michael.santos@gmail.com>
2013-11-23 10:33:22 -0500

    Clean up whitespace

--- src/rss.erl
+++ src/rss.erl
@@ -1,21 +1,21 @@
-%% Copyright (c) 2010-2012, Michael Santos <michael.santos@gmail.com>
+%% Copyright (c) 2010-2013, Michael Santos <michael.santos@gmail.com>
 %% All rights reserved.
-%% 
+%%
 %% Redistribution and use in source and binary forms, with or without
 %% modification, are permitted provided that the following conditions
 %% are met:
-%% 
+%%
 %% Redistributions of source code must retain the above copyright
 %% notice, this list of conditions and the following disclaimer.
-%% 
+%%
 %% Redistributions in binary form must reproduce the above copyright
 %% notice, this list of conditions and the following disclaimer in the
 %% documentation and/or other materials provided with the distribution.
-%% 
+%%
 %% Neither the name of the author nor the names of its contributors
 %% may be used to endorse or promote products derived from this software
 %% without specific prior written permission.
-%% 
+%%
 %% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 %% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 %% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -63,7 +63,7 @@ match([], _, Acc) ->
 match([H|T], Digest, Acc) ->
     error_logger:info_report([{rss, H}, {phash, erlang:phash2(H)}, {digest, Digest}]),
     case erlang:phash2(H) of
-        Digest -> 
+        Digest ->
             Acc;
         _ ->
             match(T, Digest, [H|Acc])
